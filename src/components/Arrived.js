d29a03ab0f6d23b0aedf5a8bb21919035c7bc320
Adnan Radja M <adnanocode@gmail.com>
2021-04-25 21:45:34 +0700

    Dukungan offline dengan workbox

--- src/components/Arrived.js
+++ src/components/Arrived.js
@@ -43,7 +43,7 @@ export default function Arrived() {
               </div>
               <h5 className="text-lg font-semibold mt-4">Cangkir Mauttie</h5>
               <span className="">IDR 89.300</span>
-              <a href="details.html" className="stretched-link"></a>
+              <a href="#details.html" className="stretched-link"></a>
             </div>
 
             <div className="px-4 relative card group">
@@ -74,7 +74,7 @@ export default function Arrived() {
               </div>
               <h5 className="text-lg font-semibold mt-4">Bankyu Minimay</h5>
               <span className="">IDR 67.899</span>
-              <a href="details.html" className="stretched-link"></a>
+              <a href="#details.html" className="stretched-link"></a>
             </div>
 
             <div className="px-4 relative card group">
@@ -105,7 +105,7 @@ export default function Arrived() {
               </div>
               <h5 className="text-lg font-semibold mt-4">Buku Sidu Edition</h5>
               <span className="">IDR 120.699</span>
-              <a href="details.html" className="stretched-link"></a>
+              <a href="#details.html" className="stretched-link"></a>
             </div>
 
             <div className="px-4 relative card group">
@@ -136,7 +136,7 @@ export default function Arrived() {
               </div>
               <h5 className="text-lg font-semibold mt-4">Watch Notes X</h5>
               <span className="">IDR 1.973.000</span>
-              <a href="details.html" className="stretched-link"></a>
+              <a href="#details.html" className="stretched-link"></a>
             </div>
 
             <div className="px-4 relative card group">
@@ -167,7 +167,7 @@ export default function Arrived() {
               </div>
               <h5 className="text-lg font-semibold mt-4">Racking Plants</h5>
               <span className="">IDR 599.400</span>
-              <a href="details.html" className="stretched-link"></a>
+              <a href="#details.html" className="stretched-link"></a>
             </div>
 
             <div className="px-4 relative card group">
@@ -198,7 +198,7 @@ export default function Arrived() {
               </div>
               <h5 className="text-lg font-semibold mt-4">Racking Plants</h5>
               <span className="">IDR 599.400</span>
-              <a href="#details.html" className="stretched-link"></a>
+              <a href="##details.html" className="stretched-link"></a>
             </div>
 
             <div className="px-4 relative card group">
@@ -229,7 +229,7 @@ export default function Arrived() {
               </div>
               <h5 className="text-lg font-semibold mt-4">Racking Plants</h5>
               <span className="">IDR 599.400</span>
-              <a href="#details.html" className="stretched-link"></a>
+              <a href="##details.html" className="stretched-link"></a>
             </div>
           </div>
         </div>
